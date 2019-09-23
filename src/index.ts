a0bd0a8008c0f6ea7937993ff9f1cd812e8cbbd4
Daniel Dietrich <cafebab3@gmail.com>
2019-09-23 15:11:06 +0200

    Flattened nested functions

--- src/index.ts
+++ src/index.ts
@@ -8,31 +8,33 @@ export = function mergeObjects<T extends Array<Obj | undefined>>(...objects: T):
 
     return objects.reduce(merge, {}) as {} & Intersection<T>;
 
-    function merge(target: Obj, source?: Obj): Obj {
-        if (source) {
-            for (const [key, value2] of Object.entries(source)) {
-                if (value2 !== undefined) {
-                    const value1 = target[key];
-                    if (value1 === undefined) { // redundant to last else case but may be faster
-                        target[key] = value2;
-                    } else if (Array.isArray(value1) && Array.isArray(value2)) {
-                        target[key] = value1.concat(value2);
-                    } else if (isObject(value1) && isObject(value2)) {
-                        target[key] = merge(value1, value2);
-                    } else {
-                        target[key] = value2;
-                    }
+};
+
+function merge(target: Obj, source?: Obj): Obj {
+    if (source) {
+        for (const [key, value2] of Object.entries(source)) {
+            if (value2 !== undefined) {
+                const value1 = target[key];
+                if (value1 === undefined) { // redundant to last else case but may be faster
+                    target[key] = value2;
+                } else if (Array.isArray(value1) && Array.isArray(value2)) {
+                    target[key] = value1.concat(value2);
+                } else if (isObject(value1) && isObject(value2)) {
+                    target[key] = merge(value1, value2);
+                } else {
+                    target[key] = value2;
                 }
             }
         }
-        return target;
     }
+    return target;
+}
 
-    function isObject(value: unknown): value is Obj {
-        return value instanceof Object && value.constructor === Object;
-    }
-};
+function isObject(value: unknown): value is Obj {
+    return value instanceof Object && value.constructor === Object;
+}
 
+// don't export these ad-hoc types!
 type Obj = Record<string | number | symbol, unknown>;
 
 // see https://stackoverflow.com/a/51604379/1110815
